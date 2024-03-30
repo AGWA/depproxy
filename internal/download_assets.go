7e686f17aedd0e5d4b021d2e322a5755e5233521
Andrew Ayer <agwa@andrewayer.name>
2024-03-30 11:26:36 -0400

    Ensure curl errors are reported when downloading assets

--- internal/download_assets.go
+++ internal/download_assets.go
@@ -2,6 +2,6 @@
 
 package depproxy
 
-//go:generate curl --silent --output assets/highlightjs.css https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css
-//go:generate curl --silent --output assets/diff2html.css https://cdn.jsdelivr.net/npm/diff2html/bundles/css/diff2html.min.css
-//go:generate curl --silent --output assets/diff2html.js https://cdn.jsdelivr.net/npm/diff2html/bundles/js/diff2html-ui.min.js
+//go:generate curl --silent --show-error --output assets/highlightjs.css https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css
+//go:generate curl --silent --show-error --output assets/diff2html.css https://cdn.jsdelivr.net/npm/diff2html/bundles/css/diff2html.min.css
+//go:generate curl --silent --show-error --output assets/diff2html.js https://cdn.jsdelivr.net/npm/diff2html/bundles/js/diff2html-ui.min.js
